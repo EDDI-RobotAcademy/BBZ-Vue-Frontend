dbabd7084168364a625b40329662c9aaa8d3cd62
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-06-14 20:00:20 +0900

    add: [이경민] Django와의 연결 [BBZV-1]

---
+++ src/board/store/mutations.ts
@@ -0,0 +1,15 @@
+import { MutationTree } from "vuex";
+import { BoardState, Board } from "./states";
+import { 
+    
+} from "./mutation-types";
+
+export interface BoardMutations extends MutationTree<BoardState> {
+    
+}
+
+const mutations: MutationTree<BoardState> = {
+    
+}
+
+export default mutations as BoardMutations
