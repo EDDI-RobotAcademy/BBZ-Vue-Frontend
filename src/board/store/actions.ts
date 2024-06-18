f52c468a214cf294a042564ac493ee8d90237010
ruvi <60618330+RUVIST@users.noreply.github.com>
2024-06-18 17:34:37 +0900

    feat: [최명근] 보드 게시글 세부 페이지 접속 구현 [BBZV-4]

--- src/board/store/actions.ts
+++ src/board/store/actions.ts
@@ -5,6 +5,7 @@ import axiosInst from "@/utility/axiosInstance"
 import { REQUEST_BOARD_LIST_TO_DJANGO } from "./mutation-types"
 
 export type BoardActions = {
+    requestBoardToDjango(context: ActionContext<BoardState, any>, boardId: number): Promise<void>
     requestBoardListToDjango(context: ActionContext<BoardState, any>): Promise<void>
     requestCreateBoardToDjango(context: ActionContext<BoardState, unknown>, payload: {
         title: string, writer: string, content: string
@@ -21,6 +22,16 @@ const actions: BoardActions = {
 
         } catch (error) {
             console.error('requestBoardListToDjango(): ' + error)
+            throw error
+        }
+    },
+    async requestBoardToDjango(context: ActionContext<BoardState, any>, boardId: number): Promise<void> {
+        try {
+            const res: AxiosResponse<Board> = await axiosInst.djangoAxiosInst.get(`/board/read/${boardId}`);
+            console.log('data:', res.data)
+            context.commit('REQUEST_BOARD_TO_DJANGO', res.data);
+        } catch (error) {
+            console.error('requestBoardToDjango() 문제 발생:', error);
             throw error
         }
     },
