5852bc8aa75f26b714104f6d9241c50251d87720
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-18 18:50:51 +0900

    feat: [한재혁] 네비게이션바 구성 완료 [BBZV-7]

---
+++ src/navigationBar/NavigationMenuBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app-bar color="orange" app dark height="64">
+        <v-btn @click="goToHome">
+            <v-toolbar-title class="text-uppercase text--darken-4">
+                <span>BBZ Hotel</span>
+            </v-toolbar-title>
+        </v-btn>
+        <v-spacer></v-spacer>
+
+        <v-btn text @click="goToProductList" class="btn-text">
+            <v-icon left>mdi-bed</v-icon>
+            <span>호텔 예약</span>
+        </v-btn>
+        <v-btn text @click="goToBoardList" class="btn-text">
+            <v-icon left>mdi-forum</v-icon>
+            <span>게시판</span>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+import '@mdi/font/css/materialdesignicons.css'
+import router from '@/router'
+
+export default {
+    data () {
+        return {
+            navigation_drawer: false,
+        }
+    },
+    methods: {
+        goToHome () {
+            router.push('/product/list')
+        },
+        goToProductList () {
+            router.push('/product/list')
+        },
+        goToBoardList () {
+            router.push('/board/list')
+        },
+    },
+}
+</script>
