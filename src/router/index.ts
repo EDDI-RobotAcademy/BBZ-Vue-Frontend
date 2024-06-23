8d1392b56ffdbadf050f02c2af7ede7b4cd092a7
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-06-24 02:07:42 +0900

    feat: [이경민] 첫 화면인 서비스 소개 페이지 생성 [BBZV-15]

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+import HomeRoutes from '@/home/router/HomeRoutes'
 import BoardRoutes from '@/board/router/BoardRoutes'
 import ProductRoutes from '@/product/router/ProductRoutes'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
+  ...HomeRoutes,
   ...BoardRoutes,
   ...ProductRoutes,
 ]
