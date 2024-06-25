d36cce256c9108d9a7c19003b90daa95a235912c
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-25 17:53:38 +0900

    fix: [한재혁] 존재하지 않는 사진이름 수정 [BBZVPx]

--- src/home/pages/HomeView.vue
+++ src/home/pages/HomeView.vue
@@ -56,12 +56,12 @@ export default {
   data () {
     return {
       hotelList: [
-        { id: 1, name: 'Photo 1', imageUrl: 'hotel-image.jpg' },
-        { id: 2, name: 'Photo 2', imageUrl: 'hotel-image.jpg' },
-        { id: 3, name: 'Photo 3', imageUrl: 'hotel-image.jpg' },
-        { id: 4, name: 'Photo 4', imageUrl: 'hotel-image.jpg' },
-        { id: 5, name: 'Photo 5', imageUrl: 'hotel-image.jpg' },
-        { id: 6, name: 'Photo 6', imageUrl: 'hotel-image.jpg' },
+        { id: 1, name: 'Photo 1', imageUrl: 'hotel1.jpeg' },
+        { id: 2, name: 'Photo 2', imageUrl: 'hotel2.jpeg' },
+        { id: 3, name: 'Photo 3', imageUrl: 'hotel3.jpeg' },
+        { id: 4, name: 'Photo 4', imageUrl: 'hotel4.jpeg' },
+        { id: 5, name: 'Photo 5', imageUrl: 'hotel5.jpeg' },
+        { id: 6, name: 'Photo 6', imageUrl: 'hotel6.jpeg' },
       ],
       hoveredHotelId: null,
     }
@@ -91,7 +91,7 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url('@/assets/images/fixed/hotel-image.jpg');
+  background-image: url('@/assets/images/fixed/hotel_main.png');
   background-size: cover;
   background-position: center;
 }
