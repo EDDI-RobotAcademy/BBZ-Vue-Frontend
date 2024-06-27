2a1c8145fa5dab1b23f7b64a2a13977a045eabb1
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-06-27 20:20:33 +0900

    style: [이경민] NavigationBar 색상 및 글꼴 변경[BBZV-7]

--- src/home/pages/HomeView.vue
+++ src/home/pages/HomeView.vue
@@ -72,7 +72,8 @@ export default {
 <style>
 /* 구글 폰트 임포트 */
 @import url( 
-  'https://fonts.googleapis.com/css2?family=Taviraj:wght@400;800&display=swap',
+  'https://fonts.googleapis.com/css2?family=Taviraj:wght@400;800&display=swap');
+@import url(
   'https://fonts.googleapis.com/css2?family=Hahmlet:wght@100..900&family=Taviraj:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
   );
 
