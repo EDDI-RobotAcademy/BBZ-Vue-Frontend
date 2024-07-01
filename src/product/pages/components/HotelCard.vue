8168aa2c8a81fc4856457add4daccceddff97b45
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-01 17:19:30 +0900

    fix: [한재혁] 호텔 예약 페이지에 가격와 세부정보 표시되게 변경 + 등록 버튼 위치 변경 [BBZV-13]

--- src/product/pages/components/HotelCard.vue
+++ src/product/pages/components/HotelCard.vue
@@ -6,7 +6,7 @@
       <v-col cols="4" class="text-center">
         <!-- 이미지만 제일 왼쪽으로 보내고 -->
         <!-- 이미지가 상단부에 붙어버리는 상황 발생하는 -->
-          <v-img :src="getImageUrl(hotel.productImageName)" height="250px"></v-img>
+        <v-img :src="getImageUrl(hotel.productImageName)" height="250px"></v-img>
       </v-col>
       <!-- 이미지 오른쪽에 해당하는 부분 -->
       <v-col>
@@ -24,13 +24,18 @@
           </v-col>
           <v-col>
             <v-text style="color:red; font-weight:bold">
-              가격정보
+              {{ hotel.productPrice }}
             </v-text>
           </v-col>
         </v-row>
         <v-row>
           <v-card-text style="background-color: #F5F5F5">
-            호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오세부우 정보오호텔 세부우 정보오호텔 세부우 정보오호텔 세부우 정보오
+            <v-row>
+              편의시설 : {{ hotel.productActivity }}
+            </v-row>
+            <v-row>
+              레스토랑 : {{ hotel.productDining }}
+            </v-row>
           </v-card-text>
         </v-row>
       </v-col>
