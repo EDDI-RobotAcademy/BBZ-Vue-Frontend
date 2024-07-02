a0836086921c15ea82a7c6572e020958dc6e4620
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-02 19:07:41 +0900

    style: [한재혁] 게시글 목록 페이지 폰트 수정 [BBZVPx]

--- src/board/pages/list/BoardListPage.vue
+++ src/board/pages/list/BoardListPage.vue
@@ -1,6 +1,10 @@
 <template lang="">
     <v-container>
-        <h2>BZZ HOTEL</h2>
+        <v-row align="center" justify="center">
+            <h2 class="title_text1">Q</h2>
+            <span class="title_text2">&</span>
+            <h2 class="title_text1">A</h2>
+        </v-row>
         <div style="text-align: left; margin: 15px;">
             <router-link :to="{ name: 'BoardRegisterPage' }">
                 게시물 작성
@@ -35,25 +39,25 @@ export default {
     },
     computed: {
         ...mapState(boardModule, ['boards']),
-        pagedItems () {
+        pagedItems() {
             const startIdx = (this.pagination.page - 1) * this.perPage
             const endIdx = startIdx + this.perPage
             return this.boards.slice(startIdx, endIdx)
         }
     },
-    mounted () {
+    mounted() {
         this.requestBoardListToDjango()
     },
     methods: {
         ...mapActions(boardModule, ['requestBoardListToDjango']),
-        readRow (event, { item }) {
+        readRow(event, { item }) {
             this.$router.push({
                 name: 'BoardReadPage',
                 params: { boardId: item['boardId'].toString() }
             })
         }
     },
-    data () {
+    data() {
         return {
             headerTitle: [
                 {
