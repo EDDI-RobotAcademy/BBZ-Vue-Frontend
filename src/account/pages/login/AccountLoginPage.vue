0aefcdb88bb12ce238ffa972f890e0cc57f73e16
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-03 14:41:39 +0900

    style: [한재혁] 호텔 세부사항에서 사용할 글씨체 스타일 추가 [BBZVPx]

--- src/account/pages/login/AccountLoginPage.vue
+++ src/account/pages/login/AccountLoginPage.vue
@@ -2,7 +2,7 @@
     <v-container fluid fill-height class="mt-15">
         <v-row align="center" justify="center">
             <span class="title_text1">S</span>
-            <span class="title_text2">IGN &nbsp;</span>  
+            <span class="title_text2">IGN &nbsp;</span>
             <span class="title_text1">I</span>
             <span class="title_text2">N</span>
         </v-row>
@@ -11,35 +11,27 @@
                 <v-card>
                     <v-card-text>
                         <v-form>
-                            <v-text-field
-                                v-model="email" label="Email" name="email"
-                                prepend-icon="mdi-account"
-                                type="text" required
-                            ></v-text-field>
-                            <v-text-field
-                                v-model="password" label="Password" name="password"
-                                prepend-icon="mdi-lock"
-                                type="password" required
-                            ></v-text-field>
+                            <v-text-field v-model="email" label="Email" name="email" prepend-icon="mdi-account"
+                                type="text" required></v-text-field>
+                            <v-text-field v-model="password" label="Password" name="password" prepend-icon="mdi-lock"
+                                type="password" required></v-text-field>
                         </v-form>
-                    <v-row align="center" justify="center" class="mt-5 mb-5">
-                        <v-btn  :style="{ backgroundColor: '#333', color: 'white', borderRadius: '0' }"
+                        <v-row align="center" justify="center" class="mt-5 mb-5">
+                            <v-btn :style="{ backgroundColor: '#333', color: 'white', borderRadius: '0' }"
                                 @click="selflogin">Login</v-btn>
-                    </v-row>
+                        </v-row>
                     </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
-                <v-row align="center" justify="center" class="mt-15">
-                    <v-col cols="auto">
-                        <p>다른 방법으로 로그인  |</p>
-                    </v-col>
-                    <v-col cols="auto" @click="goToKakaoLogin" style="cursor: pointer;">
-                        <img class="icon"
-                            :src="require('@/assets/images/fixed/kakao_login.png')"
-                            height="50">
-                    </v-col>
-                </v-row>
+        <v-row align="center" justify="center" class="mt-15">
+            <v-col cols="auto">
+                <p>다른 방법으로 로그인 |</p>
+            </v-col>
+            <v-col cols="auto" @click="goToKakaoLogin" style="cursor: pointer;">
+                <img class="icon" :src="require('@/assets/images/fixed/kakao_login.png')" height="50">
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -49,7 +41,7 @@ import { useStore } from 'vuex'
 const authenticationModule = 'authenticationModule'
 
 export default {
-    setup () {
+    setup() {
         const store = useStore()
 
         const goToKakaoLogin = async () => {
@@ -64,7 +56,7 @@ export default {
             selflogin
         }
     },
-    data () {
+    data() {
         return {
             email: '',
             password: ''
@@ -79,20 +71,28 @@ export default {
 .login_icons {
     margin-top: 20px;
 }
+
 .icons {
     cursor: pointer;
 }
+
 .title_text1 {
-  text-align: center;
-  font-size: 100px;
-  font-family: 'Gloock';
-  color: #333;
+    text-align: center;
+    font-size: 100px;
+    font-family: 'Gloock';
+    color: #333;
 }
+
 .title_text2 {
-  text-align: center;
-  margin-top: 10px;
-  font-size: 90px;
-  font-family: 'Gloock';
-  color: #333;
+    text-align: center;
+    margin-top: 10px;
+    font-size: 90px;
+    font-family: 'Gloock';
+    color: #333;
+}
+
+.bold-font {
+    font-weight: bolder;
+    font-size: 20px;
 }
 </style>
